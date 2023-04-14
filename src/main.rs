96a9dd49aae2e57a6c158b7058ab8b5964d67501
cofeek-codes <kormyshev11@mail.ru>
2023-04-14 14:11:58 +0500

    working on

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::{collections::HashMap, process::exit};
+use std::{error::Error, process::exit};
 
 use rand::seq::SliceRandom;
+
+// math
+fn factorial(num: usize) -> usize {
+    match num {
+        0 => 1,
+        1 => 1,
+        _ => factorial(num - 1) * num,
+    }
+}
+
+// math
 
 fn generate_combination(input: &Vec<i32>) -> Vec<i32> {
     let mut output: Vec<i32> = Vec::new();
@@ -13,43 +24,34 @@ fn generate_combination(input: &Vec<i32>) -> Vec<i32> {
     return output;
 }
 
-fn combos_cmp(a: Vec<i32>, b: Vec<i32>) -> bool {
-    let cmp = a.iter().zip(&b).filter(|&(a, b)| a == b).count();
+fn count_unique(input: &Vec<i32>) -> usize {
+    let mut tmpd: Vec<i32> = Vec::new();
 
-    if cmp == b.len() {
-        true
+    for el in input {
+        if !tmpd.contains(el) {
+            tmpd.push(*el);
+        }
+    }
+    tmpd.len()
+}
+
+fn count_possible_combinations(n: usize, m: usize) -> Option<usize> {
+    if m == n {
+        Some(factorial(n))
+    } else if m == 1 {
+        Some(1)
     } else {
-        false
+        let mut i = 1;
+        let mut del = 1;
+        let mut oldn = factorial(n);
+
+        while i < m {
+            oldn = oldn / del;
+            del = del + 1;
+            i = i + 1;
+        }
+        Some(oldn)
     }
-}
-
-fn factorial(num: usize) -> usize {
-    match num {
-        0 => 1,
-        1 => 1,
-        _ => factorial(num - 1) * num,
-    }
-}
-
-pub fn contains_duplicate(vec: &Vec<usize>) -> bool {
-    let mut map = HashMap::<usize, usize>::new();
-    for (i, num) in vec.iter().enumerate() {
-        match map.contains_key(num) {
-            true => return true,
-            false => map.insert(*num, i),
-        };
-    }
-    false
-}
-
-fn count_possible_combinations(input: &mut Vec<i32>) -> usize {
-    let c = input.clone();
-
-    input.dedup();
-
-    let same = (c.len() - input.len()) + 1;
-
-    return factorial(same);
 }
 
 fn main() {
@@ -58,11 +60,11 @@ fn main() {
     let args = match std::env::args().nth(1) {
         Some(args) => args,
         None => {
-            eprintln!("no argument provided");
-            exit(1);
+            "123".to_string()
+            // eprintln!("no argument provided");
+            // exit(1);
         }
     };
-
     for e in args.split("") {
         match e.parse::<i32>() {
             Ok(iel) => input.push(iel),
@@ -70,14 +72,13 @@ fn main() {
         }
     }
 
-    let mut global: Vec<Vec<i32>> = Vec::new();
-    println!("cpc: {}", count_possible_combinations(&mut input));
-
-    while global.len() < 6 {
-        let combination = generate_combination(&input);
-        if !global.contains(&combination) {
-            global.push(combination);
-        }
-    }
-    println!("global: {:?}", global);
+    // let mut global: Vec<Vec<i32>> = Vec::new();
+    // while global.len() < 6 {
+    //     let combination = generate_combination(&input);
+    //     if !global.contains(&combination) {
+    //         global.push(combination);
+    //     }
+    // }
+    // println!("global: {:?}", global);
+    println!("{:?}", count_possible_combinations(5, 4));
 }
--- src/set.js
+++
@@ -1,3 +0,0 @@
-let arg = process.argv[2]
-
-return new Set(arg.split('').forEach(e => +e)).size
